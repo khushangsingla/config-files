0d296bf6630231f515b25dbffc2a45273c82df19
Khushang Singla <khushangsingla@gmail.com>
2023-11-13 23:17:31 +0530

    Added IITB VPN files

---
+++ .config/i3/scripts/vpn_connect_iitb.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sudo bash
+openvpn --config /home/khushangsingla/.config/i3/scripts/vpn_files/IITBADV.ovpn --auth-user-pass /home/khushangsingla/.config/i3/scripts/vpn_files/creds-iitb.txt
