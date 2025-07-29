e9173c39deac57cfe51f138aefc9f802e0de6ba5
Khushang Singla <khushangsingla@gmail.com>
2025-07-29 19:18:49 +0530

    Removed unnecessary files

--- .config/i3/scripts/vpn_connect_iitb.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/sudo bash
-openvpn --config /home/khushangsingla/.config/i3/scripts/vpn_files/IITBADV.ovpn --auth-user-pass /home/khushangsingla/.config/i3/scripts/vpn_files/creds-iitb.txt
